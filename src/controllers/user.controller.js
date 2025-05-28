43963b060862bd60f2643a721e2132de79229369
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-05-28 21:22:31 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -481,4 +481,3 @@ const getWatchHistory = asynchandler(async(req,res) =>{
 
 export {registerUser,loginuser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUseravatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory}
 
-// this is a usercontroller
